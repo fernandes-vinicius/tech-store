4685c018a718ec2d363cbe32632812d5578432f7
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-02-25 17:50:47 -0300

    feat: add header menu

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,58 @@
-import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from 'lucide-react'
 
 import { Card } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 
 export function Header() {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
-      <Button aria-label="Toggle Menu" size="icon" variant="outline">
-        <MenuIcon className="size-4" />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button aria-label="Toggle Menu" size="icon" variant="outline">
+            <MenuIcon className="size-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-2">
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogInIcon className="size-4" />
+              Fazer Login
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <HomeIcon className="size-4" />
+              Início
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <PercentIcon className="size-4" />
+              Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrderedIcon className="size-4" />
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="text-lg font-semibold">
         <span className="uppercase text-primary">Tech</span> Store
