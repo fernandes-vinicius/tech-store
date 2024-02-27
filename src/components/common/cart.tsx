539306e09a025f1eb4739cf1de8b6c1a8be2f24a
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-02-27 16:52:04 -0300

    feat: add cart summary and submit button

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -5,6 +5,8 @@ import { ShoppingCartIcon } from 'lucide-react'
 import { CartItem } from '@/components/common/cart-item'
 import { CartSummary } from '@/components/common/cart-summary'
 import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { useCart } from '@/providers/cart-provider'
 import { computeProductTotalPrice } from '@/lib/utils'
 
@@ -12,7 +14,7 @@ export function Cart() {
   const { products } = useCart()
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         variant="outline"
         className="w-fit gap-1 border-primary px-3 py-1.5 text-base font-bold uppercase"
@@ -21,22 +23,30 @@ export function Cart() {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
+      <div className="h-full max-h-full overflow-hidden">
         {products.length === 0 && (
           <p className="text-center font-semibold">
             Carrinho vazio. Vamos fazer compras?
           </p>
         )}
 
-        {products.map((product) => (
-          <CartItem
-            key={product.id}
-            product={computeProductTotalPrice(product) as never}
-          />
-        ))}
-
-        <CartSummary />
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-5">
+            {products.map((product) => (
+              <CartItem
+                key={product.id}
+                product={computeProductTotalPrice(product) as never}
+              />
+            ))}
+          </div>
+        </ScrollArea>
       </div>
+
+      <CartSummary />
+
+      <Button type="button" className="font-bold uppercase">
+        Finalizar compra
+      </Button>
     </div>
   )
 }
