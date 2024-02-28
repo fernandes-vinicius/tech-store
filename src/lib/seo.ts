39eb437adcec2c11c5d1e333d4b0e6fb77f1919b
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-02-28 11:17:43 -0300

    refactor: update app description

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -2,6 +2,6 @@ import { env } from '@/lib/env'
 
 export const SEO = {
   name: 'Tech Store',
-  description: 'Sua loja de tecnologia',
+  description: 'Seu e-commerce especializado em tecnologia!',
   url: env.NEXT_PUBLIC_APP_URL,
 } as const
