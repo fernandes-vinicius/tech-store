837f66fdf4d1bc2eb107f8bb2f71272c24ee0cce
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-03-03 15:26:34 -0300

    feat: make catalog page responsive on desktop

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -16,18 +16,23 @@ export default async function CatalogPage() {
   const categories = await db.category.findMany()
 
   return (
-    <main role="main" id="catalog" className="flex flex-col gap-8 p-5">
-      <Badge variant="heading">
-        <LayoutGridIcon className="size-4 fill-foreground" />
-        Catálogo
-      </Badge>
+    <main role="main" id="catalog" className="p-5 lg:container lg:py-10">
+      <div className="flex flex-col gap-8 lg:gap-10">
+        <Badge variant="heading">
+          <LayoutGridIcon className="size-4 fill-foreground lg:size-5" />
+          Catálogo
+        </Badge>
 
-      <div className="grid grid-cols-2 gap-4">
-        {categories.map((category) => (
-          <Link key={category.id} href={`/category/${category.slug}/products`}>
-            <CategoryItem category={category} />
-          </Link>
-        ))}
+        <div className="grid grid-cols-2 gap-4 lg:grid-cols-3 lg:gap-10">
+          {categories.map((category) => (
+            <Link
+              key={category.id}
+              href={`/category/${category.slug}/products`}
+            >
+              <CategoryItem category={category} />
+            </Link>
+          ))}
+        </div>
       </div>
     </main>
   )
