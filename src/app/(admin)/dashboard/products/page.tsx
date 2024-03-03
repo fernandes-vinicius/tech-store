94e013733fd23e13d42da2057195f409ec6e5de6
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-03-03 16:50:55 -0300

    feat: show button to add product and total products on dashboard products page

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,18 +1,29 @@
-import { ProductItem } from '@/components/common/product-item'
+import { PackageIcon, PlusIcon } from 'lucide-react'
+
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
 import { db } from '@/lib/prisma'
-import { computeProductTotalPrice } from '@/lib/utils'
 
 export default async function ProductsPage() {
   const products = await db.product.findMany()
 
   return (
-    <main className="grid grid-cols-1 gap-8 lg:grid-cols-6">
-      {products.map((product) => (
-        <ProductItem
-          key={product.id}
-          product={computeProductTotalPrice(product)}
-        />
-      ))}
+    <main className="flex flex-col gap-10 p-10">
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-6">
+        <Badge variant="heading">
+          <PackageIcon className="size-5" />
+          Produtos
+        </Badge>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <h2 className="text-lg font-bold">Produtos: {products.length}</h2>
+
+        <Button>
+          <PlusIcon className="size-5" />
+          Adicionar Produto
+        </Button>
+      </div>
     </main>
   )
 }
